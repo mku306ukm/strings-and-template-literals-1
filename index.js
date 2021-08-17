acbe79d64b34e08b85616f1d3b6bbabe7f3553e4
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-08-17 02:17:34 -0700

    strings-and-template-literals-1

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('hello')
+
+const firstName = 'jonas';
+const job = 'teacher';
+const birthYear = 1994;
+const year = 2021;
+
+
+ const jonasNew = `I'm  ${firstName},a ${year-birthYear} year old ${job}!`;
+ console.log(jonasNew);
+document.write(jonasNew);
+
+console.log(`just a regular String......`)
+
+console.log(`string
+multiple 
+line`)
+
+console.log(`string \n\
+multiple \n\
+line`)
+
